a4e1a95ab73eefe9c7d0e9467d170e32d9ac58f0
jrojas <jorge.l.rojas@gmail.com>
2014-01-21 14:51:05 -0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,128 @@
+var htmlQuiz = (function() {
+ 
+    // public variables for Quiz
+   
+    var numberCorrectAnswer = 0;
+    var questionNumber = 0;
+    var liOpen = '<ol><li>';
+    var liClose ='</li></ol>';
+  
+    
+    
+    
+    //Questions Array
+    var questions = [
+        {
+        question: "What does HTML stand for?",
+        choices: ["Hyperlinks and Text Markup Language", "Hyper Text Markup Language", "Home Tool Markup Language"],
+        questionNumber : 0,
+        correctAnswer : 2,
+        
+        },
+        {
+        question: "Who is making the Web standards?",
+        choices: ["The World Wide Web Consortium", "Microsoft", "Google"],
+        questionNumber : 1,
+        correctAnswer : 1,
+        
+        },
+        {
+        question: "Choose the correct HTML tag for the largest heading",
+        choices: ["<head>", "<h1>", "<heading>"],
+        questionNumber : 2,
+        correctAnswer: 1,
+        
+        },
+        {
+        question: "What is the correct HTML tag for inserting a line break?",
+        choices: ["<lb>", "<br>", "<break>"],
+        questionNumber : 3,
+        correctAnswer: 1,
+        },
+        {
+        question: "What is the preferred way for adding a background color in HTML?",
+        choices: ["<background>yellow</background>", "background-color:yellow;","body background:yellow;"],
+        questionNumber: 4,
+        correctAnswer : 1,
+      
+    }]
+    
+        
+    // public init method to be exposed to the document ready function 
+    var init = function(){
+        firstQuestion();
+        submitAnswer();
+        validateAnswer();
+        
+  
+    };
+    
+    
+    
+    var firstQuestion = function(){
+         var questionDiv = $('.question');
+         questionDiv.append(liOpen + questions[questionNumber].question + liClose);
+        
+    
+    };
+    
+    
+    
+    
+    
+    
+    // Submit Answer
+    var submitAnswer = function() {
+        
+    $(form).on("click", ".btnSubmit", function() {
+        validateAnswer();
+        questionNumber++;
+        newQuestion();
+       
+       
+    });
+       
+        
+    };
+    
+       
+      //validateAnswer
+    var  validateAnswer = function() {
+       
+        console("Validate");
+        
+     
+        
+    };
+    
+    //new question
+    var  newQuestion = function() {
+        console("newQuestion");
+   
+    };
+    
+    
+    
+  
+ 
+    // public API
+    return {
+        init: init
+      
+        
+       
+        
+    };
+ 
+})();
+
+$(document).ready(function()
+{
+    htmlQuiz.init();
+    console.log(htmlQuiz.init);
+   
+
+});
+     
+
+ 
---
+++ readme.txt
@@ -0,0 +1,16 @@
+A web-based Quiz app using JavaScript, HTML, and CSS. This app allows the user to take a multiple choice quiz.
+
+Requirements
+
+Lead the user through a set of questions. The user should only see one question at a time, with new answers presented only after they have answered the current one. You should use jQuery to move between questions as the user enters their response
+Allow the user to input his or her response to each questions with radio buttons for the user to enter their response
+When the user answers the last question, the quiz should show his or her overall score
+Each question should be stored as a JavaScript object, and you’ll want to store your list of questions in an array. For example: var questions = [{question: "What is the capital of the US", choices: ["Washington DC", "NYC", "Silicon Valley"], correct:0}];
+Let the user know where in the quiz they are at each step (i.e. “Question 3 of 5) (Optional)
+Let the user know their score so far (Optional)
+Let the user know if their previous response was correct (Optional)
+Any useful features you can come up with! (Optional)
+
+Working Hyperlink
+
+http://jrojas.github.io/Quiz/index.html
